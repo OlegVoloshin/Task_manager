9a7b12a8c34ee2ea43cb9a3a1792978163aa2215
OlegVoloshyn <sparco2405@gmail.com>
2020-04-21 14:32:28 +0300

    Add controller & view for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,18 @@
 class TasksController < ApplicationController
+
+  def create
+    @project = Project.find(params[:project_id])
+    @project.tasks.new(task_params)
+
+    redirect_to project_path(@project)    
+  end
+
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description, :priority, :deadline, :done)
+  end
+
 end
