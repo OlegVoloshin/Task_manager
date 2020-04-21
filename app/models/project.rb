bc62241cc85effce8fa0c9f1860f9977b6879d4b
OlegVoloshyn <sparco2405@gmail.com>
2020-04-21 16:47:52 +0300

    Add some validations for project and task

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 140 }
+  validates :name, presence: true, length: { minimum: 3 }
   has_many :tasks, dependent: :destroy
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
+  validates :description, presence: true, length: { maximum: 4000 }
+  validates :priority, presence: true, numericality: { only_integer: true }
+  validates :deadline, presence: true
+  validates :done, inclusion: { in: [ true, false ] }
   belongs_to :project
 end
